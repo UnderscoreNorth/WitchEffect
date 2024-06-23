ee30c608ae831874303fdaf19828ad657eb01572
ALEX-PC\Alex <alex_nguyen@live.ca>
2024-06-22 21:47:29 -0400

    Removing haruka plushie now that I have it.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,37 @@
-<script>   
+<script>
 </script>
-<div id='container'>
-    <div id='navBar'>
-            [<a href='/'>Witch Effect</a>] - [<a href='/_n'>_n</a>|<a href='/pedals'>pedals</a>|<a href='/sprites'>sprites</a>|<a href='/letsdraw'>let's draw</a>|<a href='https://witcheffect.com/a-stream'>stream</a>]
-    </div>
-    <div id='contents'>
-    <slot />
-    </div>
+
+<div id="container">
+	<div id="navBar">
+		[<a href="/">Witch Effect</a>] - [<a href="/_n">_n</a>|<a href="/pedals">pedals</a>|<a
+			href="/sprites">sprites</a
+		>|<a href="/letsdraw">let's draw</a>|<a href="https://witcheffect.com/a-stream">stream</a>]
+	</div>
+	<div id="contents">
+		<slot />
+	</div>
 </div>
+
 <style>
-    #container{
-        height:calc(100svh - 20px);
-        width:calc(100vw - 20px);
-        padding:10px;
-        display:flex;
-        flex-direction: column;        
-    }
-    #navBar{
-        font-size:150%;
-        font-weight: bold;
-        display:flex;
-        width:100%;
-        flex-wrap: wrap;
-    }
-    #contents{
-        font-size:100%;
-        flex-grow: 1;
-        flex-shrink: 1;
-        overflow-y: auto;
-        width:100%;
-    }
+	#container {
+		height: calc(100svh - 20px);
+		width: calc(100vw - 20px);
+		padding: 10px;
+		display: flex;
+		flex-direction: column;
+	}
+	#navBar {
+		font-size: 150%;
+		font-weight: bold;
+		display: flex;
+		width: 100%;
+		flex-wrap: wrap;
+	}
+	#contents {
+		font-size: 100%;
+		flex-grow: 1;
+		flex-shrink: 1;
+		overflow-y: auto;
+		width: 100%;
+	}
 </style>
